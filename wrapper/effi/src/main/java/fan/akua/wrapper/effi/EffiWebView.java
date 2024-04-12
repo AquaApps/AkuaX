ee185df61eae49c7d840b7f4933fb2b8b67c6d5b
A-kua <732102386@qq.com>
2024-04-12 10:06:43 +0800

    update: add thread check before addIdleHandler

--- wrapper/effi/src/main/java/fan/akua/wrapper/effi/EffiWebView.java
+++ wrapper/effi/src/main/java/fan/akua/wrapper/effi/EffiWebView.java
@@ -5,14 +5,18 @@ import android.os.Looper;
 import android.webkit.WebView;
 
 class EffiWebView {
-    private static boolean webViewLoaded;
+    private static volatile boolean webViewLoaded;
 
     static void preInit(Application application) {
-        Looper.myQueue().addIdleHandler(() -> {
-            if (webViewLoaded) return false;
-            new WebView(application);
-            webViewLoaded = true;
-            return false;
-        });
+        if (!webViewLoaded){
+            if (Looper.myLooper()==Looper.getMainLooper()) {
+                Looper.myQueue().addIdleHandler(() -> {
+                    if (webViewLoaded) return false;
+                    new WebView(application);
+                    webViewLoaded = true;
+                    return false;
+                });
+            }
+        }
     }
 }
